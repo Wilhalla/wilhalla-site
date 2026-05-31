4bad041a7ae082e903d95f09feb36aeeb937fdf8
Jan-Peter Dhalle <janpeter.dhalle@gmail.com>
2026-06-01 00:47:39 +0200

    nitro vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
   server: {
     watch: {
-      ignored: [
-        "**/.dolt/**",
-        "**/.roam/**",
-        "**/.doltcfg/**",
-      ],
+      ignored: ["**/.dolt/**", "**/.roam/**", "**/.doltcfg/**"],
     },
   },
 });
